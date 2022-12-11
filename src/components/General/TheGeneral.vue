41e5cb02e177a11ffeca99a1f5d85db310f9709f
Dima Kobzar <kobzar7dima@gmail.com>
2022-12-11 14:25:58 +0200

    made animation of following after mouse text more smoother

--- src/components/General/TheGeneral.vue
+++ src/components/General/TheGeneral.vue
@@ -5,7 +5,8 @@ import "./General.scss";
 
 <template>
   <!-- костыль так как не смог придумать событие загрузки страницы -->
-  <main v-on:mousemove="test">
+  <!-- <main v-on:mousemove="test"> -->
+  <main>
     <section class="section">
       <div class="container">
         <TheIntro />
@@ -16,35 +17,58 @@ import "./General.scss";
 
 <script>
 export default {
-  data() {
-    return {};
+  mounted() {
+    const container = document.querySelector("main");
+    console.log(container);
+    const animateit = function (e) {
+      const title = document.querySelector(".intro__title");
+
+      const { offsetX: x, offsetY: y } = e;
+      const { offsetWidth: width, offsetHeight: height } = this;
+
+      let move = 25;
+      let xMove = (x / width) * (move * 2) - move;
+      let yMove = (y / height) * (move * 2) - move;
+
+      title.style.transform = `translate(${xMove}px, ${yMove}px)`;
+
+      if (e.type === "mouseleave") {
+        title.style.transform = "";
+      }
+    };
+
+    container.addEventListener("mousemove", animateit);
+    container.addEventListener("mouseleave", animateit);
   },
-  methods: {
-    //  анимация очень ломаная и дерганая
-    test() {
-      // console.log("gg");
-      const container = document.querySelector("main");
-      // console.log(container);
-      const animateit = function (e) {
-        const title = document.querySelector(".intro__title");
+  // data() {
+  //   return {};
+  // },
+  // methods: {
+  //   //  анимация очень ломаная и дерганая особенно на маленьком экране и она просто перестает работать иногда
+  //   test() {
+  //     console.log("gg");
+  //     const container = document.querySelector("main");
+  //     // console.log(container);
+  //     const animateit = function (e) {
+  //       const title = document.querySelector(".intro__title");
 
-        const { offsetX: x, offsetY: y } = e;
-        const { offsetWidth: width, offsetHeight: height } = this;
+  //       const { offsetX: x, offsetY: y } = e;
+  //       const { offsetWidth: width, offsetHeight: height } = this;
 
-        let move = 25;
-        let xMove = (x / width) * (move * 2) - move;
-        let yMove = (y / height) * (move * 2) - move;
+  //       let move = 25;
+  //       let xMove = (x / width) * (move * 2) - move;
+  //       let yMove = (y / height) * (move * 2) - move;
 
-        title.style.transform = `translate(${xMove}px, ${yMove}px)`;
+  //       title.style.transform = `translate(${xMove}px, ${yMove}px)`;
 
-        if (e.type === "mouseleave") {
-          title.style.transform = "";
-        }
-      };
+  //       if (e.type === "mouseleave") {
+  //         title.style.transform = "";
+  //       }
+  //     };
 
-      container.addEventListener("mousemove", animateit);
-      container.addEventListener("mouseleave", animateit);
-    },
-  },
+  //     container.addEventListener("mousemove", animateit);
+  //     container.addEventListener("mouseleave", animateit);
+  //   },
+  // },
 };
 </script>

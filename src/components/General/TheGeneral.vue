ef41753c3866c0a04433f7f0f39ced802bca55ea
Dima Kobzar <dimakobzar@Dimas-MacBook-Pro.local>
2022-12-17 20:24:55 +0200

    move text animation try

--- src/components/General/TheGeneral.vue
+++ src/components/General/TheGeneral.vue
@@ -5,16 +5,67 @@ import "./Logo.scss";
 </script>
 
 <template>
-  <main>
+  <main @mousemove="mouseData" ref="container">
     <section class="section">
       <div class="container">
-        <TheIntro />
+        <!-- <TheIntro v-bind:test="{ coordinateX: 12 }" /> -->
+        <!-- если координаты работают то допустим что контейнер мне не нужен -->
+        <TheIntro v-bind:coordinates="{ coordinateX, coordinateY }" />
       </div>
     </section>
   </main>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      coordinateX: 0,
+      coordinateY: 0,
+      clientWidth: 0,
+      clientHeight: 0,
+      // el: this.$refs.container,
+    };
+  },
+  methods: {
+    mouseData(e) {
+      const { offsetX: x, offsetY: y, view: cx, view: ch } = e;
+      this.coordinateX = x;
+      this.coordinateY = y;
+      this.clientWidth = cx.innerWidth;
+      this.clientHeight = ch.innerHeight;
+      // console.log(typeof this.coordinateY);
+      // console.log(this.data);
+      // console.log(e.view.innerHeight, e.view.innerWidth);
+      console.log(this.clientWidth, this.clientHeight);
+    },
+  },
+  // mounted() {
+  //   const container = document.querySelector("main");
+  //   const animateit = function (e) {
+  //     const title = document.querySelector(".intro__title");
+
+  //     const { offsetX: x, offsetY: y } = e;
+  //     const { offsetWidth: width, offsetHeight: height } = this;
+
+  //     let move = 25;
+  //     let xMove = (x / width) * (move * 2) - move;
+  //     let yMove = (y / height) * (move * 2) - move;
+
+  //     title.style.transform = `translate(${xMove}px, ${yMove}px)`;
+
+  //     if (e.type === "mouseleave") {
+  //       title.style.transform = "";
+  //     }
+  //   };
+
+  //   container.addEventListener("mousemove", animateit);
+  //   container.addEventListener("mouseleave", animateit);
+  // },
+};
+</script>
+
+<!-- <script>
 export default {
   // пробуй стоп пропагейшн чобы событие на ссылках и на спинере не ломало анимацию текста
   // и смотри модификаторы событий как capture и подобные а лучше сразу self попробуй чтобы только на мейне оно работало
@@ -41,4 +92,4 @@ export default {
     container.addEventListener("mouseleave", animateit);
   },
 };
-</script>
+</script> -->
--- src/components/Intro/TheIntro.vue
+++ src/components/Intro/TheIntro.vue
@@ -1,4 +1,5 @@
 <script setup>
+// import { numberLiteralTypeAnnotation } from "@babel/types";
 import { RouterLink } from "vue-router";
 import TheHeader from "../Header/TheHeader.vue";
 import RunningText from "../RunningText/TheRunningText.vue";
@@ -7,7 +8,7 @@ import "./Intro.scss";
 </script>
 
 <template>
-  <div class="intro">
+  <div class="intro" @mousemove="testt">
     <TheHeader />
     <div class="intro__front">
       <RunningText />
@@ -32,6 +33,50 @@ import "./Intro.scss";
     >
       What?
     </RouterLink>
-    <RouterLink to="/who" class="intro__vertical-link">Who?</RouterLink>
+    <RouterLink to="/who" class="intro__vertical-link"
+      >Who? {{ coordinates }}</RouterLink
+    >
   </div>
 </template>
+
+<script>
+export default {
+  // props: ["test"],
+  props: {
+    coordinates: Object,
+  },
+  data() {},
+  methods: {
+    testt() {
+      // const { offsetWidth: width, offsetHeight: height } = this;
+      // console.log(width, height, this);
+    },
+  },
+  mounted() {
+    const { offsetWidth: width, offsetHeight: height } = this;
+    console.log(width, height, this);
+  },
+  // mounted() {
+  //   const container = document.querySelector("main");
+  //   const animateit = function (e) {
+  //     const title = document.querySelector(".intro__title");
+
+  //     const { offsetX: x, offsetY: y } = e;
+  //     const { offsetWidth: width, offsetHeight: height } = this;
+
+  //     let move = 25;
+  //     let xMove = (x / width) * (move * 2) - move;
+  //     let yMove = (y / height) * (move * 2) - move;
+
+  //     title.style.transform = `translate(${xMove}px, ${yMove}px)`;
+
+  //     if (e.type === "mouseleave") {
+  //       title.style.transform = "";
+  //     }
+  //   };
+
+  //   container.addEventListener("mousemove", animateit);
+  //   container.addEventListener("mouseleave", animateit);
+  // },
+};
+</script>
